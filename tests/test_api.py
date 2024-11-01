0ba234a02dff1d31e2928d3120330a1340c480e8
zhangqiang <749965808@qq.com>
2024-11-01 16:57:56 +0800

    支持websocket请求

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,13 +1,16 @@
 # tests/test_api.py
 import pytest
 import allure
-from utils.request_handler import send_request
-from utils.data_loader import load_csv_data, load_json_data, load_excel_data
+from utils.request_handler import send_request, send_websocket_message_sync
 
 @allure.feature('User API')
 @allure.story('Get User Info')
-@pytest.mark.parametrize("user_id, expected_status, expected_id", load_csv_data('data/test_data.csv'))
-def test_get_user_info_csv(user_id, expected_status, expected_id):
+@pytest.mark.parametrize("user_id, expected_status, expected_id", [
+    (1, 200, 1),
+    (2, 200, 2),
+    (3, 404, None),
+])
+def test_get_user_info(user_id, expected_status, expected_id):
     url = f"https://api.example.com/user/{user_id}"
     response = send_request("GET", url)
     
@@ -20,4 +23,12 @@ def test_get_user_info_csv(user_id, expected_status, expected_id):
         else:
             assert response.json().get("id") is None
 
-# You can similarly use load_json_data or load_excel_data for other tests
+@allure.feature('WebSocket API')
+@allure.story('Send and Receive Message')
+def test_websocket_message():
+    uri = "wss://example.com/socket"
+    message = "Hello, WebSocket!"
+    
+    with allure.step("Send and receive WebSocket message"):
+        response = send_websocket_message_sync(uri, message)
+        assert response == "Expected response"
--- utils/request_handler.py
+++ utils/request_handler.py
@@ -1,6 +1,20 @@
 # utils/request_handler.py
 import requests
+import websockets
+import asyncio
 
 def send_request(method, url, **kwargs):
+    """Send HTTP requests."""
     response = requests.request(method, url, **kwargs)
     return response
+
+async def send_websocket_message(uri, message):
+    """Send a message over WebSocket."""
+    async with websockets.connect(uri) as websocket:
+        await websocket.send(message)
+        response = await websocket.recv()
+        return response
+
+def send_websocket_message_sync(uri, message):
+    """Synchronous wrapper for sending WebSocket messages."""
+    return asyncio.run(send_websocket_message(uri, message))
